0b6e223936c24f019546117e7de30d257c78f586
o̊ <67451715+j0lms@users.noreply.github.com>
2023-02-25 22:41:27 -0400

    Update vars.py

--- vars.py
+++ vars.py
@@ -44,33 +44,17 @@ emotion_labels = {
 account_list = [
 
             'SylviaPlathBot',
-            'DailyKerouac',
-            'carsonbot',
-            'williamblakebot',
-            'AllWittgenstein',
-            'Rumi_Quote',
-            'BashoSociety',
-            'botvirginia',
-            'cavafybot',
-            'marxhaunting',
-            'Scruton_Quotes',
-            'TheMarkTwain',
-            'Kurt_Vonnegut',
-            'DebsEbooks',
-            'hbloomquotes',
-            'TAMinusContext',
-            'k_punk_unlife',
 
         ]
 
-csvdir = '/datasets' # This is where all the traning data goes
+csvdir = '/datasets'
 
 test_string = 'This is a test string'
 
 tweet_number = 10
 iteration_number = 500
 
-# Hyperparameters
-EPOCHS = 10 # epoch
-LR = 5  # learning rate
-BATCH_SIZE = 64 # batch size for training
+
+EPOCHS = 10
+LR = 5
+BATCH_SIZE = 64
